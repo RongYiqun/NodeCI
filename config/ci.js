05dec8ba0cb71bb6db6494ba136945b7d491844d
Yiqun Rong <411640583@qq.com>
2020-06-11 18:19:58 +1000

    travis config

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+      '563818273986-l3mpi3l2572m0l97pb9oobaqf029tmc6.apps.googleusercontent.com',
+    googleClientSecret: 'f8c0apNFjagMnZoEiE-rkGoG',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+    cookieKey: '123123123',
+    redisUrl : 'redis://127.0.0.1:6379'
+};
--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,7 @@ module.exports = {
     '563818273986-l3mpi3l2572m0l97pb9oobaqf029tmc6.apps.googleusercontent.com',
   googleClientSecret: 'f8c0apNFjagMnZoEiE-rkGoG',
   mongoURI: 'mongodb://Yiqun:q9EbF577pfEOVJ3L@cluster0-shard-00-00-gifeu.mongodb.net:27017,cluster0-shard-00-01-gifeu.mongodb.net:27017,cluster0-shard-00-02-gifeu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl : 'redis://127.0.0.1:6379'
 };
 
